f23284310021a860816fb3760772b1b43ccc4cce
Lucas Vinícius <lvjs@academy.cin.ufpe.br>
2020-03-16 09:51:15 -0300

    first commit

---
+++ busy/Assets.xcassets/busy/busy/ViewController.dataset/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  busy
+//
+//  Created by Lucas Vinícius José da Silva on 13/03/20.
+//  Copyright © 2020 Lucas Vinícius José da Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
---
+++ busy/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  busy
+//
+//  Created by Lucas Vinícius José da Silva on 13/03/20.
+//  Copyright © 2020 Lucas Vinícius José da Silva. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ViewController: UIViewController {
+
+    @IBOutlet var listaHorarios: UILabel!
+    @IBOutlet var cronometroLabel: UILabel!
+    @IBOutlet var firstView: UIView!
+    @IBOutlet var horarioInput: UITextField!
+    var timer = Timer()
+    var count:Int=0
+    
+    @IBAction func confirmarButton(_ sender: Any) {
+    horarios.removeFirst();
+    iniciarCronometro(index: 0)
+    }
+    var horarios:[Int] = []
+    @IBAction func addHorario(_ sender: Any) {
+        
+        let horatext = horarioInput.text
+        let auxHora = horatext?.components(separatedBy: ":")
+        let hora    = auxHora?[0] ?? "0"
+        let minuto = auxHora?[1] ?? "0"
+        let intHora = Int(hora) ?? 0
+        let intMinuto = Int(minuto) ?? 0
+        horarios.append(intHora*60 + intMinuto)
+        print(horarios)
+        horarioInput.text = ""
+    }
+    
+    @IBAction func iniciarTimer(_ sender: Any) {
+        firstView.isHidden = true;
+        iniciarCronometro(index: 0);
+    }
+    func iniciarCronometro(index:Int){
+        count = horarios[index]
+        scheduledTimerWithTimeInterval()
+    }
+    func scheduledTimerWithTimeInterval(){
+
+           timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+           }
+       
+    @objc func updateTime() {
+
+       if(count > 0){
+           var minutes = String(count / 60)
+           var seconds = String(count % 60)
+           if(minutes.count<2){
+               minutes = "0"+minutes
+           }
+           if(seconds.count<2){
+               seconds = "0"+seconds
+           }
+           cronometroLabel.text = minutes + ":" + seconds
+           count-=1
+     }
+
+       }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
