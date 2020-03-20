b0da22c1a4c3f145264c3cf95588cdbc0a366a04
Lucas Vinícius José da Silva <lucasvinicius@Suportes-MacBook-Pro.local>
2020-03-20 15:16:00 -0300

    correcoes de listagem e reset de cronometro

---
+++ busy/File.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//  busy
+//
+//  Created by Lucas Vinícius José da Silva on 19/03/20.
+//  Copyright © 2020 Lucas Vinícius José da Silva. All rights reserved.
+//
+
+import Foundation
+
--- busy/ViewController.swift
+++ busy/ViewController.swift
@@ -5,7 +5,6 @@
 //  Created by Lucas Vinícius José da Silva on 13/03/20.
 //  Copyright © 2020 Lucas Vinícius José da Silva. All rights reserved.
 //
-
 import UIKit
 import Foundation
 
@@ -15,8 +14,11 @@ class ViewController: UIViewController {
     @IBOutlet var cronometroLabel: UILabel!
     @IBOutlet var firstView: UIView!
     @IBOutlet var horarioInput: UITextField!
+    @IBOutlet weak var warning: UILabel!
     var timer = Timer()
     var count:Int=0
+    var acumuladorStr:String = ""
+    var horariosString:[String] = []
     
     @IBAction func confirmarButton(_ sender: Any) {
     horarios.removeFirst();
@@ -24,16 +26,22 @@ class ViewController: UIViewController {
     }
     var horarios:[Int] = []
     @IBAction func addHorario(_ sender: Any) {
-        
-        let horatext = horarioInput.text
-        let auxHora = horatext?.components(separatedBy: ":")
-        let hora    = auxHora?[0] ?? "0"
-        let minuto = auxHora?[1] ?? "0"
-        let intHora = Int(hora) ?? 0
-        let intMinuto = Int(minuto) ?? 0
-        horarios.append(intHora*60 + intMinuto)
-        print(horarios)
-        horarioInput.text = ""
+        if(horarios.count<5){
+            let horatext = horarioInput.text
+            acumuladorStr = acumuladorStr + (horatext ?? " ") + "\n"
+            print(acumuladorStr)
+            listaHorarios.text = acumuladorStr
+            let auxHora = horatext?.components(separatedBy: ":")
+            let hora    = auxHora?[0] ?? "0"
+            let minuto = auxHora?[1] ?? "0"
+            let intHora = Int(hora) ?? 0
+            let intMinuto = Int(minuto) ?? 0
+            horarios.append(intHora*60 + intMinuto)
+            horarioInput.text = ""
+        }else{
+            horarioInput.text = ""
+            warning.isHidden = false
+        }
     }
     
     @IBAction func iniciarTimer(_ sender: Any) {
@@ -41,17 +49,23 @@ class ViewController: UIViewController {
         iniciarCronometro(index: 0);
     }
     func iniciarCronometro(index:Int){
-        count = horarios[index]
+        let date = Date()
+        let calender = Calendar.current
+        let components = calender.dateComponents([.hour,.minute], from: date)
+        let hour = components.hour
+        let minute = components.minute
+        count = horarios[index] - (hour ?? 1)*60 - (minute ?? 0)
+        count = count * 60
         scheduledTimerWithTimeInterval()
     }
     func scheduledTimerWithTimeInterval(){
 
-           timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
-           }
-       
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        
+       }
     @objc func updateTime() {
 
-       if(count > 0){
+       if(count >= 0){
            var minutes = String(count / 60)
            var seconds = String(count % 60)
            if(minutes.count<2){
@@ -67,9 +81,7 @@ class ViewController: UIViewController {
        }
     override func viewDidLoad() {
         super.viewDidLoad()
+        warning.isHidden = true
         
     }
-
-
 }
-
